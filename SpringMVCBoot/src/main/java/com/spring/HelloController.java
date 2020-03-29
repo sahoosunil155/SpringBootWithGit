943cf71042ad82c1f9871a89a9a6ec9af7eb9e3a
sahoosunil155 <sahoosunil155@gmail.com>
2020-03-29 15:46:04 +0800

    added projet ingit

---
+++ SpringMVCBoot/src/main/java/com/spring/HelloController.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping(value="hi")
+	public String sayHello()
+	{
+		return "hello";
+	}
+
+}
---
+++ SpringMVCBoot/src/main/webapp/hello.jsp
@@ -0,0 +1,1 @@
+<h1>  Welcome In Spring Boot World</h1>
